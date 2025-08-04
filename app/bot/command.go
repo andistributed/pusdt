af98c6ed98a9b649555883a71716b1ed763499b3
Hank Shen <swh@admpub.com>
2025-08-05 00:39:02 +0800

    update

--- app/bot/command.go
+++ app/bot/command.go
@@ -20,7 +20,7 @@ const cmdStart = "start"
 const cmdState = "state"
 const cmdOrder = "order"
 
-const replayAddressText = "🚚 请发送需要添加的钱包地址(可以用“钱包名称:钱包地址”这种格式来指定名称)"
+const replayAddressText = "🚚 请发送需要添加的钱包地址，也可以用“钱包名称:钱包地址”这种格式来指定名称"
 const orderListText = "*现有订单列表，点击可查看详细信息，不同颜色对应着不同支付状态！*\n>🟢收款成功 🔴交易过期 🟡等待支付 ⚪️订单取消\n>🌟按钮内容 订单创建时间 订单号末八位 交易金额"
 const orderPageSize = 8
 
