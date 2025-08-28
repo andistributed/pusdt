29cc842f4375dde520c267f7150367b9af9e3e50
Hank Shen <swhbox@foxmail.com>
2025-08-28 11:52:35 +0800

    update

--- build-linux64.sh
+++ build-linux64.sh
@@ -1,5 +1,7 @@
+export CGO_ENABLED=0
+
 export GOOS=linux
 export GOARCH=amd64
 export COMMIT=`git rev-parse --short HEAD`
 mkdir ./dist &> /dev/null
-go build -o ./dist/bepusdt_${GOOS}_${GOARCH} --trimpath --ldflags="-X main.COMMIT=${COMMIT} -w -s" ./main
+go build -o ./dist/bepusdt_${GOOS}_${GOARCH} --trimpath --ldflags="-X main.COMMIT=${COMMIT} -s -w" ./main
